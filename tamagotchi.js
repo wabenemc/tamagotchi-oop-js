98a44b6116a92caa967c83df6e987801ac12af4e
Wabene cedric <cedric.wabenemayele.simplon@gmail.com>
2024-02-22 11:15:14 +0100

    js

--- tamagotchi.js
+++ tamagotchi.js
@@ -2,4 +2,29 @@ console.log('tamagotchi file is loaded');
 
 class Tamagotchi {
   // code of the class goes here
+
+  // Creation class et constructor
+  constructor() {
+      this.foodInTummy = 10;
+      this.restedness = 10;
+      this.health = 10;
+  }
+// Fonction cry 
+  cry() {
+      this.foodInTummy--;
+      console.log("New food in tummy:", this.foodInTummy);
+      console.log("WAHH !!");
+  }
 }
+
+
+// Nouveau ta Tamagotchi
+let tamagotchi1 = new Tamagotchi();
+let tamagotchi2 = new Tamagotchi();
+tamagotchi1.cry();
+tamagotchi2.cry();
+console.log(tamagotchi1);
+console.log(tamagotchi2);
+
+
+
